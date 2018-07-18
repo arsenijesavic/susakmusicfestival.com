29b5cb3a761894723340062aae0b26b3980e9429
Arsenije Savic <djxbserbia@gmail.com>
2018-07-18 10:14:47 +0200

    Updates

---
+++ component---src-pages-404-js-806d2338b0416df33759.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{212:function(e,t,n){"use strict";n.r(t);var a=n(1),u=n.n(a);t.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-806d2338b0416df33759.js.map
--- src/sections/index.js
+++
@@ -1,5 +0,0 @@
-import Hero from './1. Hero'
-import Program from './2. Program'
-import Artists from './3. Artists'
-
-export { Hero, Program, Artists }
